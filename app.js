f13c3386ab4d520b299a511543c574bee863d15d
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-02-22 19:18:45 +0000

    created fetchWeather function

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+// Save URL to variable
+const apiEndpoint =
+	"https://api.open-meteo.com/v1/forecast?latitude=50.8284&longitude=-0.1395&current=temperature_2m,apparent_temperature,precipitation,rain,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m&hourly=uv_index&daily=weather_code,uv_index_max&timezone=GMT";
 
+// Create async function to deal with api request
+async function fetchWeather(callback) {
+	const response = await fetch(apiEndpoint, { method: "GET" });
+
+	if (!response.ok) {
+		console.error(response.status);
+		console.error(response.text());
+		return;
+	}
+	// parse the response
+	const data = await response.json();
+	callback(data);
+}
