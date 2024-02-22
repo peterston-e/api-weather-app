45026434d39a8fdd22c98face76290c0cfafd4d9
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-02-22 21:22:02 +0000

    created callback function to display data

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+// ***** selectors ******
+const day = document.querySelector(".day");
+const time = document.querySelector(".time");
+const degC = document.querySelector(".degreesC");
+const precipitation = document.querySelector(".precipitation-percent");
+const humidity = document.querySelector(".humidity-percent");
+const windSpeed = document.querySelector(".wind-speed");
+
 // Save URL to variable
 const apiEndpoint =
-	"https://api.open-meteo.com/v1/forecast?latitude=50.8284&longitude=-0.1395&current=temperature_2m,apparent_temperature,precipitation,rain,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m&hourly=uv_index&daily=weather_code,uv_index_max&timezone=GMT";
+	"https://api.open-meteo.com/v1/forecast?latitude=50.8284&longitude=-0.1395&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m&hourly=uv_index&daily=weather_code,uv_index_max&timezone=GMT";
 
-// Create async function to deal with api request
+// Create async function to deal with api request - callback function as param
 async function fetchWeather(callback) {
 	const response = await fetch(apiEndpoint, { method: "GET" });
 
@@ -15,3 +23,34 @@ async function fetchWeather(callback) {
 	const data = await response.json();
 	callback(data);
 }
+
+// callback function to be used with api call
+function displayData(weatherData) {
+	const currentTime = weatherData.current.time;
+	time.textContent = currentTime;
+
+	const currentTemp = weatherData.current.temperature_2m;
+	degC.textContent = `${currentTemp} ℃`;
+
+	const currentPrecipitation = weatherData.current.precipitation;
+	precipitation.textContent = `${currentPrecipitation} %`;
+
+	const currentHumidity = weatherData.current.relative_humidity_2m;
+	humidity.textContent = `${currentHumidity} %`;
+
+	const currentWindSpeed = weatherData.current.wind_speed_10m;
+	windSpeed.textContent = `${currentWindSpeed} km/h`;
+}
+
+fetchWeather(displayData);
+
+// // Alternative to setting the callback function
+// async function useData() {
+//     try {
+//       const weatherData = await fetchWeather();
+//       // Use the weatherData object here
+
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
